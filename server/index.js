68e996fc248faf4ad80b0c26427b0573ca9afc6a
Evan Siroky <evan.siroky@yahoo.com>
2017-04-08 09:59:37 -0700

    automatically redirect to https in production

--- server/index.js
+++ server/index.js
@@ -8,6 +8,16 @@ const db = require('./models')
 
 const app = express()
 
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(['https://', req.get('Host'), req.url].join(''))
+    }
+    return next()
+  })
+}
+
+
 app.set('port', (process.env.PORT || 5000))
 
 // static assets
